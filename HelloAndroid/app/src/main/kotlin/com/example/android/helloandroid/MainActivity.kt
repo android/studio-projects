282a41c4d630b51388ad85b50fa78376f2a6f34b
Jamal Eason <pedro_sax@hotmail.com>
2025-12-06 20:57:53 -0800

    Update copyright year and top app bar colors

--- HelloAndroid/app/src/main/kotlin/com/example/android/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/kotlin/com/example/android/helloandroid/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ fun HelloAndroidApp(name: String, modifier: Modifier = Modifier) {
             topBar = {
                 CenterAlignedTopAppBar(
                     title = { Text("Android") },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer
                     )
                 )
